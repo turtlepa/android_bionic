6b8965caf8342890dc3fca2d19ab2979b22886dc
David Eddlemon <DavidEddlemon@Hotmail.com>
2013-08-16 22:21:06 +0400

    Strict Aliasing Fix
    Change-Id: Iebd61ca581c558f7097cf4db85360fb9c50a2db3

--- libc/Android.mk
+++ libc/Android.mk
@@ -467,6 +467,7 @@ libc_common_cflags := \
     -D_LIBC=1 \
     -DFLOATING_POINT \
     -DINET6 \
+    -fno-strict-aliasing \
     -I$(LOCAL_PATH)/private \
     -DPOSIX_MISTAKE \
     -DLOG_ON_HEAP_ERROR \
@@ -478,22 +479,23 @@ libc_common_cflags := \
 #
 libc_common_cflags += \
     -DTM_GMTOFF=tm_gmtoff \
-    -DUSG_COMPAT=1
+    -DUSG_COMPAT=1 \
+    -fno-strict-aliasing
 
 ifeq ($(strip $(DEBUG_BIONIC_LIBC)),true)
-  libc_common_cflags += -DDEBUG
+  libc_common_cflags += -DDEBUG -fno-strict-aliasing
 endif
 
 # To customize dlmalloc's alignment, set BOARD_MALLOC_ALIGNMENT in
 # the appropriate BoardConfig.mk file.
 #
 ifneq ($(BOARD_MALLOC_ALIGNMENT),)
-  libc_common_cflags += -DMALLOC_ALIGNMENT=$(BOARD_MALLOC_ALIGNMENT)
+  libc_common_cflags += -DMALLOC_ALIGNMENT=$(BOARD_MALLOC_ALIGNMENT) -fno-strict-aliasing
 endif
 
 ifeq ($(TARGET_ARCH),arm)
   libc_common_cflags += -DSOFTFLOAT
-  libc_common_cflags += -fstrict-aliasing
+  libc_common_cflags += -fno-strict-aliasing
   libc_crt_target_cflags := -mthumb-interwork
 endif # !arm
 
@@ -512,7 +514,7 @@ ifeq ($(TARGET_ARCH),mips)
   ifneq ($(ARCH_MIPS_HAS_FPU),true)
     libc_common_cflags += -DSOFTFLOAT
   endif
-  libc_common_cflags += -fstrict-aliasing
+  libc_common_cflags += -fno-strict-aliasing
   libc_crt_target_cflags := $(TARGET_GLOBAL_CFLAGS)
 endif # mips
 
