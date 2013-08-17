1df230f5872cbce1fb9f6ddcbecd186cc07c19b4
david lamontagne <djlamontagneiii@gmail.com>
2013-08-16 20:51:22 -0400

    do not build or enable pthread debug
    Change-Id: I6eeeeb689cce536cf330b822f6da5e0e4ee2a91e

--- libc/bionic/libc_init_dynamic.cpp
+++ libc/bionic/libc_init_dynamic.cpp
@@ -54,7 +54,6 @@
 #include <bionic_tls.h>
 
 extern "C" {
-  extern void pthread_debug_init(void);
   extern void malloc_debug_init(void);
   extern void malloc_debug_fini(void);
 };
@@ -76,7 +75,6 @@ __attribute__((constructor)) static void __libc_preinit() {
   __libc_init_common(*args);
 
   // Hooks for the debug malloc and pthread libraries to let them know that we're starting up.
-  pthread_debug_init();
   malloc_debug_init();
 }
 
